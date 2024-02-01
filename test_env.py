2ffd9670c028c32c3c76aa2b9a72f4805981228c
mnobre <mateusm.nobre@gmail.com>
2024-02-01 11:47:17 -0300

    Add files via upload

--- test_env.py
+++ test_env.py
@@ -1,8 +1,8 @@
 import numpy as np
-import Elden_Bot as Bot
+import Elden_Bot_Meta as Bot
 import time
 
 time.sleep(3.0)
-X = np.load('EldenBrain.npy')
-S = Bot.run_bot(X)
+W = np.load('MetaBrain.npy')
+S = Bot.run_bot(W)
 print(S)
